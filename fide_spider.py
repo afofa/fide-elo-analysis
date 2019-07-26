d0b7a279442ec7bff3ccc95509f6ca6ffb409c0f
afofa <ahmed.ozkalay@epfl.ch>
2019-07-26 19:29:30 +0300

    add scraper for player top pages

--- fide_spider.py
+++ fide_spider.py
@@ -7,32 +7,71 @@ class FIDE_ELO_Spider(scrapy.Spider):
 
     def parse(self, response):
         for row in response.xpath('//div[@id="main-col"]/*[@class="contentpaneopen"][2]/tr[2]/td/*[1]//tr')[1:]:
-            print(row.get())
+            # print(row.get())
             rank = int(row.xpath('td[1]/text()').get().strip())
             full_name = row.xpath('td[2]/a/text()').get().strip()
             last_name, first_name = full_name.split(', ')
-            url = self.BASE_URL + row.xpath('td[2]/a/@href').get().strip()
+            top_files_url = self.BASE_URL + row.xpath('td[2]/a/@href').get().strip()
             title = self.parse_title(row.xpath('td[3]/text()').get().strip())
             country = row.xpath('td[4]/text()').get().strip()
             elo = int(row.xpath('td[5]/text()').get().strip())
             num_of_games = int(row.xpath('td[6]/text()').get().strip())
             birth_year = int(row.xpath('td[7]/text()').get().strip())
 
-            yield {
+            scraped_data = {
                 'rank' : rank,
                 'full_name' : full_name,
                 'first_name' : first_name,
                 'last_name' : last_name,
-                'url' : url,
+                'top_files_url' : top_files_url,
                 'title' : title,
                 'country' : country,
                 'elo' : elo,
                 'num_of_games' : num_of_games,
-                'birth_year' : birth_year
+                'birth_year' : birth_year,
             }
 
-    def parse_player_profile(self, url : str) -> None:
-        pass
+            yield response.follow(url=top_files_url, callback=self.parse_top_files_url, cb_kwargs=scraped_data)
+
+    def parse_top_files_url(self, response, **kwargs) -> None:
+        rankings = []
+        for i, row in enumerate(response.xpath('//div[@id="main-col"]/*[@class="contentpaneopen"][2]/tr[2]/td/*[1]//tr')):
+            if i == 0:
+                card_url, id_url, chess_statistics_url = tuple([self.BASE_URL + i_str for i_str in row.xpath('td/a/@href').getall()])
+
+            elif i == 1:
+                pass
+
+            else:
+                ranking_name = row.xpath('td[2]/a/text()').get().strip()
+                ranking_url = self.BASE_URL + '/' + row.xpath('td[2]/a/@href').get().strip()
+                rank = int(row.xpath('td[3]/text()').get().strip())
+                title = self.parse_title(row.xpath('td[4]/text()').get().strip())
+                elo = int(row.xpath('td[5]/text()').get().strip())
+                num_of_games = int(row.xpath('td[6]/text()').get().strip())
+
+                rankings.append(
+                    {
+                        'ranking_name' : ranking_name,
+                        'ranking_url' : ranking_url,
+                        'rank' : rank,
+                        'title' : title,
+                        'elo' : elo,
+                        'num_of_games' : num_of_games,
+                    }
+                )
+
+        scraped_data = kwargs
+        scraped_data_new = {
+            'card_url' : card_url,
+            'id_url' : id_url,
+            'chess_statistics_url' : chess_statistics_url,
+            'rankings' : rankings,
+        }
+
+        scraped_data_all = {**scraped_data, **scraped_data_new}
+
+        yield scraped_data_all
 
     def parse_title(self, title_str : str) -> str:
         matched_title = ""
